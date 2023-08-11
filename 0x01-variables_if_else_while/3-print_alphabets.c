be8f3cb80e2e5a681a86af6c15cd535a29cf88b2
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-11 15:11:35 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= i; i++)
+	{
+		putchar(i);
+	}
+	puthar(10);
+	return (0);
+}
