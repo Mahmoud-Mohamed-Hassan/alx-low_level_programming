93423c7f67bbd394b50876bac8668166bcbda791
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-11 15:52:03 -0700

    numbers with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints numbers with putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i<57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+	}
+
+	putchar(10);
+	return (0);
+}
