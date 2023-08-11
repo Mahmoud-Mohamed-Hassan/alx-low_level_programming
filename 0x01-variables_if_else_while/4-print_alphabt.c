d5550741bb0c87193ddeeac10d841591fb28ee33
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-11 15:22:34 -0700

    alphabet without q or o

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with putchar function no q or o
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 113 || i == 111)
+		continue;
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
