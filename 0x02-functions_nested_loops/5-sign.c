b2538434e2df9c269000790fc1b7bb8ea929af0d
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-15 16:41:16 -0700

    sign check

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - function to check if charcter is lowercase
+ *
+ * @n: check input of function
+ *
+ * Return: return 1 if 'n' is more than 0,
+ * -1 if less, otherwise always 0 (success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+
+}
