8a2519fa1b0d20920537a3fddbfe4881d27c828f
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-15 20:17:08 -0700

    advance 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *print_times_table - print time table of n
+ *
+ * @n: input of function
+*/
+void print_times_table(int n)
+{
+	int pr, mul, number;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (number = 0; number <= n; ++number)
+		{
+			_putchar(48);
+		for (mul = 1; mul <= n; ++mul)
+			{
+			_putchar(',');
+			_putchar(' ');
+
+			pr = number * mul;
+			if (pr <= 9)
+				_putchar(' ');
+			if (pr <= 99)
+				_putchar(' ');
+			if (pr >= 100)
+			{
+				putchar((pr / 100) + 48);
+				_putchar ((pr / 10) % 10 + 48);
+			} else if (pr <= 99 && pr >= 10)
+				_putchar((pr / 10) + 48);
+			_putchar(pr % 10 + 48);
+			}
+		_putchar('\n');
+		}
+	}
+}
