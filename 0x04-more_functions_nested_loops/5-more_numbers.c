1bcc0d22905689d7671cff967df6f31b8889ee66
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-17 11:46:58 -0700

    numbers to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - utilizes the _putchar function to print
+ *                      the numbers from 0 to 14
+*/
+
+void more_numbers()
+{
+	int n, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar(n / 10 + 48);
+				_putchar(n % 10 + 48);
+			}
+			else
+				_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
