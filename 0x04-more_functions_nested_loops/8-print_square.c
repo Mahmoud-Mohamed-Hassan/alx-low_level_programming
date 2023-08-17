84a8806311010e8a5ceab23b0d7e4d105923e769
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-17 14:31:56 -0700

    # square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_square - function prints line
+* @size: input
+*/
+
+void print_square(int size)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+}
