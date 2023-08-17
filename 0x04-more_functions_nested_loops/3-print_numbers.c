b71b90990e5e0d6c30c049fb679582a9f218f65e
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-17 11:12:12 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - utilizes the _putchar function to print
+ *                      the numbers from 0 to 9
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
