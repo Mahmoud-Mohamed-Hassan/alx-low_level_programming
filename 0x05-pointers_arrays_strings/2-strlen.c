01c1c6ac5b198d7fdf8311d0c6269a7d4ced8518
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-21 14:47:36 -0700

    sting length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - return string length
+ *
+ * @s: int parameter
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *str != '\0'; *s++)
+		counter++;
+	return (counter);
+}
