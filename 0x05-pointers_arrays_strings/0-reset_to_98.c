d23b7f1e9032cb1cf2eeaada4aaad67c411b1b85
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-21 14:32:09 -0700

    derefrencing pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to int as parameter and
+ * update it's value to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
