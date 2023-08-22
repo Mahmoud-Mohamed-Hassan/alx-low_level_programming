f3bc86bfc48e2893d58dd67157c0b1567fb2eb05
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-21 19:59:42 -0700

    derefrencing pointer

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print complex password
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum;
+	char x;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		x = ran() % 128;
+		sum += x;
+		putchar(x);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
