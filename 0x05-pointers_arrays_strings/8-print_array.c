8b104c6f4a09a8ded89e303fed9e88d6c1fd4edf
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-21 18:25:07 -0700

    print half string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - print array and new line
@@ -15,9 +16,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
-			printf("%d, ",a[i]);
+			printf("%d, ", a[i]);
 		else
-			printf("%d",a[i]);
+			printf("%d", a[i]);
 	}
 	_putchar('\n');
 }
