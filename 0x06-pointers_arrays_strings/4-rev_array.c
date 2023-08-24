e2c19df0d2acccfcad41caf6fc1a201d6193ecf7
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-23 23:48:35 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ *
+ * @a: int parameter
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, i;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = a[i];
+	}
+}
