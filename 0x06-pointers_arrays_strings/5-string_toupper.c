d345696042ec045de44d8d33804c74852907c910
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-24 15:18:32 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *string_toupper - change lowercase to upper
+ *
+ * @n: int parameter
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	_putchar(n + 32);
+	return (n);
+}
