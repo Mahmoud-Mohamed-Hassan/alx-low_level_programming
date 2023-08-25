ce1aa6303209d5f83d22405c1ee83ad0218eba98
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-08-24 19:14:59 -0700

    encode rot13

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	int n1;
+
+	n1=n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar (n1 / 10 + '0');
+}
