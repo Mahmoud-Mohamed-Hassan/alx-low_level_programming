77e4a7f33763900dca3b97ae8e01c0146d3d3c26
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-09-05 12:48:34 -0700

    return dublicate pointer to array of char  with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	copy = malloc(sizeof(char) * count + 1);
 	if (copy == NULL)
 		return (NULL);
-	for (i = 0; arr[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		copy[i] = arr[i];
 	return (copy);
 }
