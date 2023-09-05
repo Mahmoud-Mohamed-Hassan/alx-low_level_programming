dc0b3540968368b7632ecb6a367ce2c1cfaab0b7
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2023-09-05 12:46:32 -0700

    return dublicate pointer to array of char  with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_strdup - returns a pointer to a newly allocated space in memory
+*@str: string to be duplicated
+*Return: pointer to a new string which is a duplicate of the string str
+*/
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (int i = 0; str[i] != '\0'; i++)
+		count++;
+	copy = malloc(sizeof(char) * count + 1);
+	if (copy == NULL)
+		return (NULL);
+	for (int i = 0; arr[i] != '\0'; i++)
+		copy[i] = arr[i];
+	return (copy);
+}
